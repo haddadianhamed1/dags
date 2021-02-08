7f9b31620dcd9985acc049074331bb90a92f4b23
haddadianhamed1 <haddadian.hamed@gmail.com>
2021-02-07 18:26:43 -0800

    updated

--- dags/hamed_dag.py
+++ dags/hamed_dag.py
@@ -1,22 +1,17 @@
-
-from datetime import datetime
+import datetime
+import logging
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators import MultiplyBy5Operator
 
-def print_hello():
- return 'Hello Wolrd'
+hello_dag = DAG(
+        "lesson1.excercise1",
+        start_date=datetime.datetime.now(),
+        schedule_interval='@daily')
 
-dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+hello_task = PythonOperator(
+        task_id="hello_world_task",
+        python_callable=hello_world,
+        dag=hello_dag)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
-
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
-multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
-                                task_id='multiplyby5_task', dag=dag)
-
-dummy_operator >> hello_operator
-
-dummy_operator >> multiplyby5_operator
+def hello_world():
+    logging.info("Hello World")
