6dc8f34195e8b173c51feb5459f0137dc28b52b7
haddadianhamed1 <haddadian.hamed@gmail.com>
2021-02-14 14:00:55 -0800

    private image

--- dags/4_alpaca_test1.py
+++ dags/4_alpaca_test1.py
@@ -29,7 +29,8 @@ with example_workflow:
 
         t1 = KubernetesPodOperator(namespace='airflow-alpaca',
                                image="hhaddadian/alpaca:v0.1.0",
-                               cmds=["python -c 'import app;app.print_account()'"],
+                               cmds=["python", "-c"],
+                               arguments=["import app", "app.print_account()"],
                                labels={'runner': 'airflow'},
                                name="airflow-test-pod",
                                image_pull_secrets="regcred",
